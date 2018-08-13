bb235a0150695ec2d0aac618ba818755dccd7cc5
Fons van der Plas <fonsvdplas@gmail.com>
2018-08-13 16:12:40 +0200

    Working version

---
+++ PoloreceiptServer/Module.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using System.IO;
+using System.Diagnostics;
+
+namespace PoloreceiptServer
+{
+	public class Module : NancyModule
+	{
+		public Module()
+		{
+			
+			var indexHtml = File.ReadAllText("index.html");
+			Get["/"] = para => {
+				return indexHtml;
+			};
+			Post["/photoupload"] = para =>
+			{
+				var file = Request.Files.FirstOrDefault();
+				if(file != null)
+				{
+					byte[] fileContents;
+					using(BinaryReader br = new BinaryReader(file.Value))
+					{
+						fileContents = br.ReadBytes((int)file.Value.Length);
+					}
+					
+					string fileName = DateTime.Now.Ticks.ToString() + file.Name.Replace(' ','-');
+					if(!Directory.Exists("photos"))
+					{
+						Directory.CreateDirectory("photos");
+					}
+					
+					File.WriteAllBytes("photos/" + fileName, fileContents);
+					RunCommand("convert", "-normalize " + "photos/" + fileName + " " + "photos/" + "n" + fileName);
+					RunCommand("lp", "photos/" + "n" + fileName);
+					
+					return "ok";
+					//return Response.AsImage("photos/" + "n" + fileName);
+					
+				}
+				return "invalid image :[";
+				return Response.AsRedirect("/");
+			};
+			Get["/snel"] = Get["/sneller"] = Get["/fast"] = Get["/faster"] = para => Response.AsRedirect("http://192.168.2.3/");
+		}
+
+		private void RunCommand(string a, string b)
+		{
+			ProcessStartInfo psi = new ProcessStartInfo(a, b);
+			psi.UseShellExecute = false;
+			psi.CreateNoWindow = true;
+			psi.RedirectStandardOutput = false;
+
+			Process proc = new Process();
+			proc.StartInfo = psi;
+
+			proc.Start();
+			proc.WaitForExit();
+		}
+	}
+}
--- PoloreceiptServer/Program.cs
+++ PoloreceiptServer/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Nancy.Hosting.Self;
 
 namespace PoloreceiptServer
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			int port = 80;
+			var url = "http://" + "localhost" + ":" + port;
+			HostConfiguration hostConfigs = new HostConfiguration();
+			hostConfigs.UrlReservations.CreateAutomatically = true;
+			using(var nancyHost = new NancyHost(hostConfigs, new Uri(url)))
+			{
+				nancyHost.Start();
+
+				Console.WriteLine("Nancy now listening - navigating to {0}. Press enter to stop", url);
+				try
+				{
+					//Process.Start("http://localhost:3579/");
+				}
+				catch(Exception)
+				{
+				}
+				Console.ReadKey();
+			}
+
+			Console.WriteLine("Stopped. Good bye!");
 		}
 	}
 }
