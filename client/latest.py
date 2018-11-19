6320c61cdf31a9280f4d74c291f4b47f3cd50b2d
Fons van der Plas <fonsvdplas@gmail.com>
2018-11-19 22:21:16 +0100

    🐮

--- client/latest.py
+++ client/latest.py
@@ -5,7 +5,7 @@ from io import BytesIO
 def setup():
     ## Printer
     printerPath = "/dev/usb/lp0"
-    fh = os.open(printerPath, 'w')
+    fh = os.open(printerPath, os.O_WRONLY)
     fh.write(bytearray([ord(x) for x in "Hello!"]))
 
     logoRequest = r.get("https://printi.me/Content/logo.h58")
