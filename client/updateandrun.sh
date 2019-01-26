8ec0b831954c4426948cf4dac424e78197d38ca8
Fons van der Plas <fonsvdplas@gmail.com>
2019-01-26 18:52:53 +0100

    Client logs 👀

--- client/updateandrun.sh
+++ client/updateandrun.sh
@@ -4,5 +4,7 @@ cd /root/printi
 ! git reset --hard
 ! git pull
 
+touch /root/printi.log
+
 (cd /root/printi/client/PrintiConfigServer/PrintiConfigServer; python3 PrintiConfigServer.py)&
-(cd /root/printi/client/PrintiClient/PrintiClient; python3 PrintiClient.py)&
+(cd /root/printi/client/PrintiClient/PrintiClient; python3 -h PrintiClient.py &> /root/printi.log)&
