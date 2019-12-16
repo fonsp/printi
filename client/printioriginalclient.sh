a9c876fe2065f12c829cc3d5ad19d2cb5b068f02
Fons van der Plas <fonsvdplas@gmail.com>
2019-12-16 01:18:14 +0100

    Update printioriginalclient.sh

--- client/printioriginalclient.sh
+++ client/printioriginalclient.sh
@@ -12,9 +12,17 @@ do
 		extension="${downloadedfile#*.}"
 		newname=$(ls .. | wc -l).$extension
 
-		mv $downloadedfile ../$newname
+		mv "$downloadedfile" ../$newname
 
 		#! python normalize_quantiles.py "$tmpdir$downloadedfile" "$tmpdirNORM$downloadedfile"
-		lp -o fit-to-page ../$newname
+		convert ../$newname -rotate "90>" ../toprint.BMP
+
+		height=$(identify -format "%h" ../toprint.BMP)
+		if [ $height -le 576 ]
+		then
+			lp -o orientation-requested=4 ../toprint.BMP
+		else
+			lp -o orientation-requested=3 ../toprint.BMP
+		fi
 	fi
 done
