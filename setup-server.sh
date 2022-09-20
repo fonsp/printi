2812c9f9cac6096004f164d4733de4a721080578
Fons van der Plas <fonsvdplas@gmail.com>
2022-09-20 23:52:20 +0200

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# FYI i never tried this script, but i hope it works!
+
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install unzip
 
-wget https://github.com/denoland/deno/releases/download/v1.18.0/deno-x86_64-unknown-linux-gnu.zip -O temp.zip
+wget https://github.com/denoland/deno/releases/download/v1.25.3/deno-x86_64-unknown-linux-gnu.zip -O temp.zip
 unzip temp.zip
 
 sudo mv ./deno /bin/deno
@@ -13,8 +16,8 @@ cd /bin/
 git clone https://github.com/fonsp/printi.git
 cd printi
 
-cp printi.service /etc/systemd/system/
-systemctl start printi
-systemctl enable printi
+sudo cp printi.service /etc/systemd/system/
+sudo systemctl start printi
+sudo systemctl enable printi
 
 sudo reboot
